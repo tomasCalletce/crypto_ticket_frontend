dfa29be727863730eabcc7fbeaa28945c91c0f40
salviega <salviega6@gmail.com>
2022-07-30 15:46:15 -0500

    [FEAT] Se adicionó IPFS
    Hay que hacer pruebas

---
+++ src/middleware/ipfs.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { IpfsClient } from 'ipfs-http-client';
+import toBuffer from 'it-to-buffer';
+
+function IpfsApi() {
+  const ipfs = IpfsClient({ host: 'ipfs.infura.io', port: '5001', protocol: 'https' });
+  const [ipfsHash, setIpfsHash] = React.useState('');
+
+  const run = async (eventInformation) => {
+    let results = [];
+
+    for await (let result of ipfs.add(eventInformation)) {
+      results.push(result);
+    }
+    console.log(results[0].cid.string); // hash;
+    setIpfsHash(results[0].cid.string);
+    const fileContents = await toBuffer(ipfs.cat(ipfsHash));
+    console.log(fileContents);
+  };
+
+  const fileSelector = (eventInformation) => {
+    run(eventInformation);
+  };
+}
+
+export default IpfsApi;
