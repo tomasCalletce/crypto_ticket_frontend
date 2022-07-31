19e2aa639833a1cf71d2a17bb214f395d45897c7
salviega <salviega6@gmail.com>
2022-07-30 20:34:03 -0500

    [FEAT] Se creó REST-API

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,35 +1,35 @@
 import React from 'react';
-import {BrowserRouter, Route, Routes} from "react-router-dom"
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import { Box } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react';
 
 import Header from '../../shared/Header';
 import Footer from '../../shared/Footer';
 
 import Home from '../Home';
-import Maker from "../Maker"
+import Maker from '../Maker';
 import About from '../About ';
 import Event from '../Event';
 
+import { getCompany } from '../../middleware/restApi';
+
 function App() {
+  getCompany();
 
-  
   return (
-    <Box w="100%" minHeight="100vh" >
-
-      <Header/>
+    <Box w='100%' minHeight='100vh'>
+      <Header />
       <BrowserRouter>
-             <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path={'/event/:id'} element={<Event />}/>
-                <Route path="/about" element={<About/>}/>
-                <Route path="/maker" element={<Maker/>}/>
-            </Routes>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path={'/event/:id'} element={<Event />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/maker' element={<Maker />} />
+        </Routes>
       </BrowserRouter>
-      <Footer/>
-      
+      <Footer />
     </Box>
-    );
+  );
 }
 
 export default App;
